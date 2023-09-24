95d729bf7bc46287dbcc9feb01e6ec1c155ee595
SafeerAhmFarooqi <90330142+SafeerAhmFarooqi@users.noreply.github.com>
2023-09-24 16:25:47 +0500

    Commit

---
+++ resources/views/layouts/app-layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @include('includes.styles.master-styles')
+    @yield('styles')
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{isset($title)?($title?:config('app.name')):config('app.name')}}</title>
+</head>
+<body>
+    {{$slot}}
+    @include('includes.scripts.master-scripts')
+    @yield('scripts')
+</body>
+</html>
---
+++ resources/views/pages/application/index.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <x-slot name="title">
+       safeer
+    </x-slot>
+<h1>ahmed farooqi</h1>
+ 
+</x-app-layout>
